b3688dc6f9e2915cab96b546fcf0149d52664eba
TAMILARASAN S <tamilarasansdd05@gmail.com>
2026-03-05 09:15:26 +0530

    Add files via upload

---
+++ parse_pdf/parse_paper.py
@@ -0,0 +1,13 @@
+import fitz  # PyMuPDF
+
+def parse_pdf(file):
+
+    pdf = fitz.open(stream=file.read(), filetype="pdf")
+
+    text = ""
+
+    for page in pdf:
+        text += page.get_text()
+
+    return text
+    
